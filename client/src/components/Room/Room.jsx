65dd65ab19ea9c2add9cef34b51cc658fb31774e
amithasanamithasan <amit35-1773@diu.edu.bd>
2024-09-14 16:57:20 +0600

    Rooms are not avialable category no data found

--- client/src/components/Room/Room.jsx
+++ client/src/components/Room/Room.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import RoomCard from "./RoomCard";
 import Container from "../Shared/Container";
 import { useSearchParams } from "react-router-dom";
+import Heading from "../Shared/Heading";
 
 
 const Room = () => {
@@ -30,12 +31,19 @@ fetch('rooms.json')
 
     return (
         <Container>
-        <div className=" pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 
+        {rooms && rooms.length>0 ? <div className=" pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 
         lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
             {
                 rooms.map(room=><RoomCard key={room._id} room={room}></RoomCard>)
             }
+        </div>: <div className=" items-center justify-between min-h-[500px] m-20">
+
+        <Heading   center={true }  
+        title="No Rooms Avilable In This Category!'" 
+        subtitle="Please Other Seleced Categories.">
+        </Heading>
         </div>
+        }
         </Container>
     );
 };
