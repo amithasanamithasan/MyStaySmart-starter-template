0c412bd64ac16c3810465175e85974929bd36fd2
amithasanamithasan <amit35-1773@diu.edu.bd>
2024-09-14 16:27:17 +0600

    By filtering the data of each room with the category of API json file ,i got the branches in individual category

--- client/src/components/Room/Room.jsx
+++ client/src/components/Room/Room.jsx
@@ -1,20 +1,32 @@
 import { useEffect, useState } from "react";
 import RoomCard from "./RoomCard";
 import Container from "../Shared/Container";
+import { useSearchParams } from "react-router-dom";
 
 
 const Room = () => {
 const [rooms , setRooms]=useState([]);
-
+const [params , setParams]=useSearchParams();
+// oonno je kono component e access korte parbo params.get kore 
+  const category= params.get('category')
 // API data loaded
 
 useEffect( ()=>{
 fetch('rooms.json')
 
 .then(res=>res.json())
-.then(data=> setRooms(data))
+.then(data=> {
+    if(category){
 
-},[])
+  const fillered= data.filter(room=> room.category=== category)
+  setRooms(fillered)
+
+    }else{
+        setRooms(data)
+    }
+})
+
+},[category])
 
     return (
         <Container>
